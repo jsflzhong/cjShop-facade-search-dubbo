2d788a74354c61ef1a1f8a8c74869719adc62789
jsflzhong <jsflzhong@163.com>
2018-05-27 08:38:29 +0800

    修改pojo的导入，改为从common-core导入。

--- src/main/java/com/cj/core/facade/search/ItemFacade.java
+++ src/main/java/com/cj/core/facade/search/ItemFacade.java
@@ -1,6 +1,6 @@
 package com.cj.core.facade.search;
 
-import com.cj.common.pojo.TaotaoResult;
+import com.cj.core.pojo.TaotaoResult;
 
 public interface ItemFacade {
 	TaotaoResult importItems() throws Exception;
